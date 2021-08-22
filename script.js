20bc1448c1a7a995c484c289bd41f8db959e3db9
JPDBrodeur <jbrodeur001@gmail.com>
2021-08-21 21:12:25 -0400

    color check every 10 seconds

--- script.js
+++ script.js
@@ -5,6 +5,22 @@ var today = moment().format("dddd, MMMM do");
 var timeArr = [9, 10, 11, 12, 13, 14, 15, 16, 17];
 var hourlyEvents;
 
+// Color code past, present and future hours
+var colorCheck = function() {
+    $.each(timeArr, function(index, hour) {
+        var minutesFrom = moment().diff(moment().hour(hour).minute(0), 'minutes');
+        if (minutesFrom < 0) {
+            $("." + hour + ".event").addClass("future");
+        } else if (minutesFrom > 59) {
+            $("." + hour + ".event").removeClass("present");
+            $("." + hour + ".event").addClass("past");
+        } else {
+            $("." + hour + ".event").removeClass("future");
+            $("." + hour + ".event").addClass("present");
+        } 
+    });
+}
+
 var loadEvents = function() {
     hourlyEvents = JSON.parse(localStorage.getItem("events"));
 
@@ -30,19 +46,13 @@ var createHourlyEventArr = function() {
 // Add date to header
 $('#currentDay').text(today);
 
-// Color code past, present and future hours
-$.each(timeArr, function(index, hour) {
-    var minutesFrom = moment().diff(moment().hour(hour).minute(0), 'minutes');
-    if (minutesFrom < 0) {
-        $("." + hour + ".event").addClass("future");
-    } else if (minutesFrom > 59) {
-        $("." + hour + ".event").addClass("past");
-    } else {
-        $("." + hour + ".event").addClass("present");
-    }
-})
-
 loadEvents();
+colorCheck();
+
+// Run colorCheck every 10 seconds
+setInterval(function() {
+    colorCheck();
+}, 1000 * 10);
 
 // save button on row was clicked
 $(document).on('click', '.saveBtn', function() {
